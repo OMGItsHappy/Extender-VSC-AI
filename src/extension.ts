bee414052eb24248ca5439708b2c6881720f7dc6
Oliver <oliverschwab03@gmail.com>
2024-01-20 13:59:50 -0700

    Added a env file.

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import OpenAI from 'openai';
+import dotenv from 'dotenv';
+import {TEST} from './env';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,7 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('open-ai-integration.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Open-AI Integration!');
+		const message = TEST || 'Default message';
+		vscode.window.showInformationMessage(message);
 	});
 
 	context.subscriptions.push(disposable);
@@ -31,6 +36,14 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable2);
+
+	let getClipboardText = vscode.commands.registerCommand('open-ai-integration.getClipboardText', () => {
+		vscode.env.clipboard.readText().then((text) => {
+			vscode.window.showInformationMessage(text);
+		});
+	});
+
+	context.subscriptions.push(getClipboardText);
 }	
 
 // This method is called when your extension is deactivated
